315a1728b346acf82ae78da49dd178e669f240fb
jacardenas <juanancardenas@gmail.com>
2025-11-23 22:10:36 +0100

    add views and router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { NotfoundComponent } from "./notfound/notfound.component";
+import { PreferencesComponent } from './preferences/preferences.component';
+import { PlayComponent } from './play/play.component';
+import { RecordsComponent } from './records/records.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'preferences', component: PreferencesComponent },
+  { path: 'play', component: PlayComponent },
+  { path: 'records', component: RecordsComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: NotfoundComponent }
+];
---
+++ src/app/records/records.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-records',
+  imports: [],
+  templateUrl: './records.component.html',
+  styleUrl: './records.component.css',
+})
+export class RecordsComponent {
+
+}
